efa1e39dba1d2cfb153d38a8387dcaef71f0485f
Shiyuan-Huang-23 <shiyuancn@live.cn>
2020-06-08 22:55:55 -0400

    Added counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Learn React and save to reload.
         </p>
+        <p>Counter: {counter}</p>
+        <button title="Count!" onClick={() => setCounter(counter + 1)} className="Button"/>
         <a
           className="App-link"
           href="https://reactjs.org"
