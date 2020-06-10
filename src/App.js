db9acb5b7e027389add52115898bb8ee0390bc6b
Shiyuan-Huang-23 <shiyuancn@live.cn>
2020-06-09 20:41:31 -0400

    Print empty tree to console using OCaml

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import Tree from './bucklescript/tree';
+import { Tree } from './ocaml_src/tree.bs';
 import './App.css';
 
 function App() {
@@ -14,19 +14,21 @@ function App() {
 
   const printEmptyTree = () => {
     Tree.print_tree(Tree.empty);
-  }
+  };
 
   const [num, setNum] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Learn React and save to reload.
-        </p>
+        <p>Learn React and save to reload.</p>
         <p>Factorial: {num}</p>
         <div>
-          <input type="number" name="Factorial" onChange={(e) => setNum(factorial(e.target.value))}/>
+          <input
+            type="number"
+            name="Factorial"
+            onChange={(e) => setNum(factorial(e.target.value))}
+          />
         </div>
         <a
           className="App-link"
@@ -36,7 +38,7 @@ function App() {
         >
           Learn React
         </a>
-        <button title="Print Tree to Console" onClick={printEmptyTree}/>
+        <button title="Print Tree to Console" onClick={printEmptyTree} />
       </header>
     </div>
   );
