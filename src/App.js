24731a22879808d34a7ea6aa1f901cb009255b6d
Vaishnavi Gupta <vg222@cornell.edu>
2020-06-21 11:59:53 -0400

    basic tooltip setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
-import { ArrowLeftOutlined } from '@ant-design/icons';
+import { Popover } from 'antd';
+import { ArrowLeftOutlined, InfoCircleOutlined } from '@ant-design/icons';
 import Context from './components/Context';
 import HomeContent from './components/HomeContent';
 import GeneratePhyloContent from './components/GeneratePhyloContent';
@@ -8,6 +9,12 @@ import VisualizePhyloContent from './components/VisualizePhyloContent';
 
 const Header = ({ useBack }) => {
   const { goHome } = React.useContext(Context);
+  const content = (
+    <div>
+      <p>Content</p>
+      <p>Content</p>
+    </div>
+  );
   return (
     <header className="site-header">
       <button className="site-title" onClick={goHome}>
@@ -18,6 +25,10 @@ const Header = ({ useBack }) => {
         ) : null}
         Phylo
       </button>
+      <Popover content={content} className="tooltip" title="Title" trigger="click">
+        <a class="tooltip"> <InfoCircleOutlined style={{ paddingRight: '0.5em' }} />Confused by all the terms?</a>
+      </Popover>
+
     </header>
   );
 };
