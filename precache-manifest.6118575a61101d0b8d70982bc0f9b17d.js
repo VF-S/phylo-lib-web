dac34fb88da4574c9496df77b893d551a2528096
Shiyuan-Huang-23 <shiyuancn@live.cn>
2020-06-30 15:32:00 -0400

    Updates

---
+++ precache-manifest.6118575a61101d0b8d70982bc0f9b17d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1180ffcbe5bd70561e29e8c1b59a6b70",
+    "url": "/phylo-lib-web/index.html"
+  },
+  {
+    "revision": "7cab1abcde8c04f9eec0",
+    "url": "/phylo-lib-web/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "67fb1a7aad394f3859e5",
+    "url": "/phylo-lib-web/static/css/main.918054c4.chunk.css"
+  },
+  {
+    "revision": "7cab1abcde8c04f9eec0",
+    "url": "/phylo-lib-web/static/js/2.e4997703.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/phylo-lib-web/static/js/2.e4997703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67fb1a7aad394f3859e5",
+    "url": "/phylo-lib-web/static/js/main.f27fc8eb.chunk.js"
+  },
+  {
+    "revision": "a44e621cdec2a1660f2c",
+    "url": "/phylo-lib-web/static/js/runtime-main.cfbdd8df.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/phylo-lib-web/precache-manifest.30cb086f26d49d5b216fbdc17f921e60.js"
+  "/phylo-lib-web/precache-manifest.6118575a61101d0b8d70982bc0f9b17d.js"
 );
 
 self.addEventListener('message', (event) => {
