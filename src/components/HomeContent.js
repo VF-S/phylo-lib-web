63104ae31aeff755321b7047fce60603c62d5e77
Shiyuan-Huang-23 <shiyuancn@live.cn>
2020-07-22 14:58:34 -0400

    Center examples in home page

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -7,9 +7,7 @@ import cytochrome_c from '../ocaml_src/Examples/cytochrome_c.js';
 import capsid from '../ocaml_src/Examples/Capsid.js';
 const { Content } = Layout;
 
-
 export default function HomeContents() {
-
   const [PhyloTree, setPhyloTree] = useState(cytochrome_c);
   const [phyloVisible, setPhyloVisible] = useState(true);
 
@@ -27,7 +25,7 @@ export default function HomeContents() {
         setPhyloTree(capsid);
         break;
     }
-  }
+  };
 
   console.log(process.env.PUBLIC_URL);
   return (
@@ -59,87 +57,85 @@ export default function HomeContents() {
             </Link>
           </Col>
         </Row>
-        <Row justify='center'>
+        <Row justify="center">
           <Divider />
           <h1> Explanations and Examples: </h1>
         </Row>
         <Row>
           <h2> About Phylogenetic Trees: </h2>
-          <h3> Scientists often wish to infer the evolutionary history between
-          different organisms. In order to determine the closeness of species
-          such as birds or fish, historically the similarity in physical
-          characteristics was analyzed. With the advent of modern computers
-          and DNA analysis, the field has moved towards using DNA.
-
-          Two species with more similar DNA are assumed to be more closely
-          related. By analyzing these similarities and differences in the DNA,
-          we can generate a hypothetical evolutionary tree, called a
-          phylogenetic tree, that estimates what the historical evolutionary
-          tree would have looked like.
+          <h3>
+            {' '}
+            Scientists often wish to infer the evolutionary history between
+            different organisms. In order to determine the closeness of species
+            such as birds or fish, historically the similarity in physical
+            characteristics was analyzed. With the advent of modern computers
+            and DNA analysis, the field has moved towards using DNA. Two species
+            with more similar DNA are assumed to be more closely related. By
+            analyzing these similarities and differences in the DNA, we can
+            generate a hypothetical evolutionary tree, called a phylogenetic
+            tree, that estimates what the historical evolutionary tree would
+            have looked like.
           </h3>
 
-          <h2> Functionality:  </h2>
+          <h2> Functionality: </h2>
 
           <h3>
             <b> Visualize Pairwise Alignment: </b>
             At the heart of constructing phylogenetic trees is the pairwise
-            alignment, where two DNA sequences are compared. Only once
-            the alignment scores of all DNA sequences with one another is known
-            is it possible to determine genetic similarity and through that,
+            alignment, where two DNA sequences are compared. Only once the
+            alignment scores of all DNA sequences with one another is known is
+            it possible to determine genetic similarity and through that,
             evolutionary history. The pairwise Alignment is performed using the
-            Needleman-Wunsch algorithm, a globally optimal dynamic
-            programming algorithm that computes a loss for the most optimal
-            alignment. Here * represents a DNA base pair match, _ a gap,
-            often used if a DNA sequence is longer than the other, and |
-            a mismatch.
+            Needleman-Wunsch algorithm, a globally optimal dynamic programming
+            algorithm that computes a loss for the most optimal alignment. Here
+            * represents a DNA base pair match, _ a gap, often used if a DNA
+            sequence is longer than the other, and | a mismatch.
           </h3>
           <h3>
             <b> Visualize Phylogenetic Trees: </b>
             When a phylogenetic tree has already been constructed, whether
-            through this program or other commercially available software,
-            it is important that the user can visualize the final alignment.
-            Here we built a tokenizer, lexer, and parser to read PhyloXML,
-            an extension of the popular XML format designed to encode
-            phylogenetic trees.
+            through this program or other commercially available software, it is
+            important that the user can visualize the final alignment. Here we
+            built a tokenizer, lexer, and parser to read PhyloXML, an extension
+            of the popular XML format designed to encode phylogenetic trees.
           </h3>
           <h3>
             <b> Generate Phylogenetic Trees: </b>
-            We construct phylogenetic genetic trees from DNA sequences
-            encoded in a standard .FASTA format using two algorithms:
-            Unweighted Pair Group Method with Arithmetic Mean (UPGMA)
-            and Bayesian Inference, each with their respective tradeoffs
-            regarding speed and accuracy. Each algorithm reads in the .FASTA
-            files, constructs pairwise alignments, and then uses respective
-            techniques to find an optimal rooted tree.
-
+            We construct phylogenetic genetic trees from DNA sequences encoded
+            in a standard .FASTA format using two algorithms: Unweighted Pair
+            Group Method with Arithmetic Mean (UPGMA) and Bayesian Inference,
+            each with their respective tradeoffs regarding speed and accuracy.
+            Each algorithm reads in the .FASTA files, constructs pairwise
+            alignments, and then uses respective techniques to find an optimal
+            rooted tree.
           </h3>
-
-          <h2> Examples: </h2>
-          <b>
-            Examples of constructed phylogenetic trees using DNA sequences
-            from the National Center for Biotechnology Information (NCBI).
-            </b>
-          <Row className="centered-content">
-            <Radio.Group onChange={changeGenerateExamples}>
-              <Radio.Button value="Virus Capsid">Virus Capsid Gene</Radio.Button>
-              <Radio.Button value="COXII">
-                Cytochrome C Oxidase Subunit II
-          </Radio.Button>
-              <Radio.Button value="Influenza A Viruses">
-                Influenza A PB-2
-          </Radio.Button>
-            </Radio.Group>
-          </Row>
-          {phyloVisible ? (
-            <Row justify="center">
-              <div className="ascii-phylo-container">
-                <p className="ascii-phylo">{PhyloTree}</p>
-              </div>
-            </Row>
-          ) : null}
         </Row>
 
+        <h2> Examples: </h2>
+
+        <h3>
+          Examples of constructed phylogenetic trees using DNA sequences from
+          the National Center for Biotechnology Information (NCBI).
+        </h3>
+        <Row className="centered-content">
+          <Radio.Group onChange={changeGenerateExamples}>
+            <Radio.Button value="Virus Capsid">Virus Capsid Gene</Radio.Button>
+            <Radio.Button value="COXII">
+              Cytochrome C Oxidase Subunit II
+            </Radio.Button>
+            <Radio.Button value="Influenza A Viruses">
+              Influenza A PB-2
+            </Radio.Button>
+          </Radio.Group>
+        </Row>
+        {phyloVisible ? (
+          <Row justify="center">
+            <div className="ascii-phylo-container">
+              <p className="ascii-phylo">{PhyloTree}</p>
+            </div>
+          </Row>
+        ) : null}
       </Content>
-    </div >
+    </div>
   );
 }
