167dfa775dc8b3f4ca21463b90861a848aa400e7
Vaishnavi Gupta <vg222@cornell.edu>
2020-07-29 15:26:30 -0400

    UPGMA + bayesian explanation

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -29,42 +29,45 @@ export default function HomeContents() {
 
   console.log(process.env.PUBLIC_URL);
   return (
-    <div className="wrapper main">
+    <div className="wrapper">
       <Content justify="center">
-        <Row className="intro" justify="center">
-          <div>
-            <h1>Welcome to PhyloML</h1>
-            <h2>A phylogenetic library written in OCaml.</h2>
-            <h2>
-              Inferring Evolutionary History through Modern Genetic Similarity.
+        <div className="home">
+          <Row className="intro" justify="center">
+            <div>
+              <h1>Welcome to PhyloML</h1>
+              <h2>A phylogenetic library written in OCaml.</h2>
+              <h2>
+                Inferring Evolutionary History through Modern Genetic Similarity.
             </h2>
-          </div>
-        </Row>
-        <Row justify="center" gutter={[16, 40]}>
-          <Col lg={7} md={10} sm={12}>
-            <Link className="offset" to="/pairwise">
-              Visualize Pairwise DNA Alignments
+            </div>
+          </Row>
+          <Row justify="center" gutter={[16, 40]}>
+            <Col lg={7} md={10} sm={12}>
+              <Link className="offset" to="/pairwise">
+                Visualize Pairwise DNA Alignments
             </Link>
-          </Col>
-          <Col lg={7} md={10} sm={12}>
-            <Link className="offset" to="/visualize">
-              Visualize Phylogenetic Trees
+            </Col>
+            <Col lg={7} md={10} sm={12}>
+              <Link className="offset" to="/visualize">
+                Visualize Phylogenetic Trees
             </Link>
-          </Col>
-          <Col lg={7} md={10} sm={12}>
-            <Link className="offset" to="/generate">
-              Generate Phylogenetic Trees
+            </Col>
+            <Col lg={7} md={10} sm={12}>
+              <Link className="offset" to="/generate">
+                Generate Phylogenetic Trees
             </Link>
-          </Col>
-        </Row>
-        <Row justify="center">
-          <Divider />
-          <h1> Explanations and Examples: </h1>
-        </Row>
-        <Row>
-          <h2> About Phylogenetic Trees: </h2>
-          <h3>
-            {' '}
+            </Col>
+          </Row>
+        </div>
+        <div className="explanation">
+          <Row justify="center">
+            <Divider />
+            <h1> Explanations and Examples </h1>
+          </Row>
+          <Row>
+            <h2> About Phylogenetic Trees: </h2>
+            <h3>
+              {' '}
             Scientists often wish to infer the evolutionary history between
             different organisms. In order to determine the closeness of species
             such as birds or fish, historically the similarity in physical
@@ -77,65 +80,66 @@ export default function HomeContents() {
             have looked like.
           </h3>
 
-          <h2> Functionality: </h2>
+            <h2> Functionality: </h2>
 
-          <h3>
-            <b> Visualize Pairwise Alignment: </b>
-            At the heart of constructing phylogenetic trees is the pairwise
-            alignment, where two DNA sequences are compared. Only once the
-            alignment scores of all DNA sequences with one another is known is
+            <h3>
+              <b> Visualize Pairwise Alignment: </b>
+            At the heart of constructing phylogenetic trees is sequence alignment, where gaps are inserted in the sequences to make them all of equal length, to get a similarity score. Only once the alignment scores of all DNA sequences with one another is known is
             it possible to determine genetic similarity and through that,
-            evolutionary history. The pairwise Alignment is performed using the
+            evolutionary history.<br></br>
+            One such method is the pairwise alignment, where two DNA sequences are aligned. It is performed using the
             Needleman-Wunsch algorithm, a globally optimal dynamic programming
             algorithm that computes a loss for the most optimal alignment. Here
-            * represents a DNA base pair match, _ a gap, often used if a DNA
-            sequence is longer than the other, and | a mismatch.
+             {" "} <span className="highlight">*</span> represents a DNA base pair match, <span className="highlight">_</span> a gap, often used if a DNA sequence is shorter than the other, and <span className="highlight">|</span> a mismatch.
           </h3>
-          <h3>
-            <b> Visualize Phylogenetic Trees: </b>
+            <h3>
+              <b> Visualize Phylogenetic Trees: </b>
             When a phylogenetic tree has already been constructed, whether
             through this program or other commercially available software, it is
             important that the user can visualize the final alignment. Here we
             built a tokenizer, lexer, and parser to read PhyloXML, an extension
             of the popular XML format designed to encode phylogenetic trees.
           </h3>
-          <h3>
-            <b> Generate Phylogenetic Trees: </b>
+            <h3>
+              <b> Generate Phylogenetic Trees: </b>
             We construct phylogenetic genetic trees from DNA sequences encoded
             in a standard .FASTA format using two algorithms: Unweighted Pair
             Group Method with Arithmetic Mean (UPGMA) and Bayesian Inference,
             each with their respective tradeoffs regarding speed and accuracy.
             Each algorithm reads in the .FASTA files, constructs pairwise
             alignments, and then uses respective techniques to find an optimal
-            rooted tree.
-          </h3>
-        </Row>
-
-        <h2> Examples: </h2>
-
-        <h3>
-          Examples of constructed phylogenetic trees using DNA sequences from
-          the National Center for Biotechnology Information (NCBI).
-        </h3>
-        <Row className="centered-content">
-          <Radio.Group onChange={changeGenerateExamples}>
-            <Radio.Button value="Virus Capsid">Virus Capsid Gene</Radio.Button>
-            <Radio.Button value="COXII">
-              Cytochrome C Oxidase Subunit II
-            </Radio.Button>
-            <Radio.Button value="Influenza A Viruses">
-              Influenza A PB-2
-            </Radio.Button>
-          </Radio.Group>
-        </Row>
-        {phyloVisible ? (
-          <Row justify="center">
-            <div className="ascii-phylo-container">
-              <p className="ascii-phylo">{PhyloTree}</p>
-            </div>
+            rooted tree.<br></br><br></br>
+            The first step in UPGMA consists of creating an <i>n</i> by <i>n</i> distance matrix which records the genetic distance between each pair of sequences in the alignment. Then, we pick the 2 sequeneces with the least distance and combine them to form a 'clade' (a branching point in the tree). Then we do the same thing with the next two most similar sequences. We keep doing this until we are left with one clade - our final phylogenetic tree. <br></br><br></br>
+            The essence of Bayesian inference is using prior information about the genetic data to predict the likelihood that a certain tree is te correct phylogenetic tree, given our aligned sequence data. Since finding this likelihood for every possible tree structure takes an unfeasibly large amount of time, we use a technique called Markov Chain Monte Carlo sampling to construct a chain of trees and get a sample of the final tree distribution. By getting a sample of the tree distribution, we can find out the most likely phylogenetic tree given our genetic data. The advantage of this method is that it can be tweaked considerably to account for various genetic substituion models, based on the existing knowledge that we have about the genetic sequences. It is, however, slower than UPGMA.
+            </h3>
           </Row>
-        ) : null}
-      </Content>
+
+          <h2> Examples: </h2>
+
+          <h3>
+            Examples of constructed phylogenetic trees using DNA sequences from
+            the National Center for Biotechnology Information (NCBI).
+        </h3>
+          <Row className="centered-content">
+            <Radio.Group onChange={changeGenerateExamples}>
+              <Radio.Button value="Virus Capsid">Virus Capsid Gene</Radio.Button>
+              <Radio.Button value="COXII">
+                Cytochrome C Oxidase Subunit II
+            </Radio.Button>
+              <Radio.Button value="Influenza A Viruses">
+                Influenza A PB-2
+            </Radio.Button>
+            </Radio.Group>
+          </Row>
+          {phyloVisible ? (
+            <Row justify="center">
+              <div className="ascii-phylo-container">
+                <p className="ascii-phylo">{PhyloTree}</p>
+              </div>
+            </Row>
+
+          ) : null}
+        </div></Content>
     </div>
   );
 }
